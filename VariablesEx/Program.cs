d18c711f851e3deb8e98c9a1dc6f9a35832040a3
Hannah Willis <hwillis8817@gmail.com>
2022-03-18 13:18:52 -0500

    completed variable exercise

---
+++ VariablesEx/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace VariablesEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "Duke";
+            int dogAge = 7;
+            bool lovesSleep = true;
+            char nickName = 'D';
+            double toys = 43;
+            decimal weight = 25.4m; 
+                
+              Console.WriteLine($"My dog's name is {dogName} aka {nickName}, He is {dogAge} years old and weighs {weight} pounds.He likes his {toys} suffed toys and sleeps a lot {lovesSleep} ");
+        }
+    }
+}
